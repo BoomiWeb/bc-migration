4a54eb2f90fa21c132627ac2939cb21faeb701b6
Erik Mitchell <erikdmitchell@gmail.com>
2025-02-20 12:52:12 -0500

    initial commit

---
+++ src/Class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Test class
+ *
+ * @package BCMigration
+ * @since   0.1.0
+ * @version 0.1.0
+ */
+
+namespace erikdmitchell\bcmigration;
+
+class TestClass {
+
+    public function __construct() {
+        echo 'Test Class';
+    }
+
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'boomi-cms-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3b3067aa6d9f41d89b13310681975acb4b656ede',
+        'name' => 'erikmitchell/bc-migration',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'erikmitchell/bc-migration' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'boomi-cms-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3b3067aa6d9f41d89b13310681975acb4b656ede',
+            'dev_requirement' => false,
+        ),
+    ),
+);
