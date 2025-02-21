49a25f127c3572784effffd72c5dd2e6a7122287
Erik Mitchell <erikdmitchell@gmail.com>
2025-02-21 12:13:25 -0500

    doc block and code clean up

--- src/cli/AITool.php
+++ src/cli/AITool.php
@@ -20,16 +20,27 @@ use WP_CLI;
 class AITool extends CLICommands {
 
     /**
-     * Construct
+     * Migrates AI Tool data.
+     *
+     * ## OPTIONS
+     *
+     * <action>
+     * : The action to perform. Currently only `reports` and `likes` are supported.
+     *
+     * ## EXAMPLES
+     *
+     *     # Migrate AI Tool reports.
+     *     $ wp migrate reports
+     *     Success: Migrated 13 reports.
+     *
+     *     # Migrate AI Tool likes.
+     *     $ wp migrate likes
+     *     Success: Migrated 13 likes.
+     *
+     *     # Remove the AI Tool uploads folder.
+     *     $ wp migrate remove-folder
+     *     Success: Removed the AI Tool uploads folder.
      */
-    public function __construct() {
-
-
-        // add_action( 'bc_legacy_tool_likes_process_complete_aitool', array( $this, 'remove_ai_tool_uploads_folder' ) );
-
-        
-    }
-
     public function migrate( $args, $assoc_args ) {
         list ( $action ) = $args;
 
@@ -43,6 +54,9 @@ class AITool extends CLICommands {
                 break;
             case 'likes':
                 $this->migrate_likes();
+                break;
+            case 'remove-folder':
+                $this->remove_folder();
                 break;
             default:
                 WP_CLI::error( 'Invalid action.' );
@@ -70,6 +84,12 @@ class AITool extends CLICommands {
         WP_CLI::success( count( $migrated_data ) . ' AI Tool reports migrated successfully.' );
     }
 
+    /**
+     * Migrates AI Tool likes to the database.
+     *
+     * This method will log messages to the user about the migration process and
+     * report the number of migrated records.
+     */
     private function migrate_likes() {
         WP_CLI::log( 'Migrating AI Tool likes...' );
 
@@ -84,43 +104,54 @@ class AITool extends CLICommands {
         WP_CLI::success( count( $migrated_data ) . ' AI Tool likes migrated successfully.' );        
     }
 
- 
-    
-    public function remove_ai_tool_uploads_folder() {
-        error_log( 'bc_update_521_remove_ai_tool_uploads_folder - false' );
-        return false;
+    /**
+     * Removes the AI Tool uploads folder.
+     *
+     * This method will log messages to the user about the deletion process.
+     *
+     * @since 0.1.0
+     */
+    private function remove_folder() {
         $upload_dir = wp_upload_dir( null, false );
-        $bc_logger  = boomi_get_logger();
-
-        $deleted = BC_Filesystem::getInstance()->delete( $upload_dir['basedir'] . '/bc-ai-tool-data' );
+        $deleted = $this->remove_directory( $upload_dir['basedir'] . '/bc-ai-tool-data' );
 
         if ( $deleted ) {
-            $bc_logger->info(
-                sprintf(
-                    __( 'AI Tool uploads folder deleted.', 'boomi-cms' )
-                ),
-                array(
-                    'source' => 'boomi-ai-tool-uploads-folder-update',
-                )
-            );
+            WP_CLI::success( 'AI Tool uploads folder deleted.' );
+
+            delete_option( '_bc_ai_tool_page_id' );
         } else {
-            $bc_logger->error(
-                sprintf(
-                    __( 'AI Tool uploads folder deletion failed.', 'boomi-cms' )
-                ),
-                array(
-                    'source' => 'boomi-ai-tool-uploads-folder-update',
-                )
-            );
+            WP_CLI::error( 'AI Tool uploads folder deletion failed.' );
         }
-
-        delete_option( '_bc_ai_tool_page_id' );
     }
-    
 
+    /**
+     * Removes a directory recursively.
+     *
+     * @param string $dir The path to the directory to be deleted.
+     *
+     * @return bool True if the directory was deleted successfully, false otherwise.
+     */
+    private function remove_directory(string $dir) {
+        if (!file_exists($dir)) {
+            return true;
+        }
     
-
+        if (!is_dir($dir)) {
+            return unlink($dir);
+        }
     
-
+        foreach (scandir($dir) as $item) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+    
+            if (!$this->remove_directory($dir . DIRECTORY_SEPARATOR . $item)) {
+                return false;
+            }
+    
+        }
+        
+        return rmdir($dir);        
+    }
 
 }
