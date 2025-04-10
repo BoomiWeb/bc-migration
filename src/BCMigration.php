cd9784a4ec8aa36a0cb03a5892b279ea1c5abe65
Erik Mitchell <erikdmitchell@gmail.com>
2025-04-10 10:08:02 -0400

    cleaned up toggleLog js func

--- src/BCMigration.php
+++ src/BCMigration.php
@@ -20,10 +20,12 @@ $example_files_path = __DIR__ . '/examples';
 
 // Define constants for the plugin.
 define( 'BCM_PATH', __DIR__ );
+define( 'BCM_URL', __DIR__ );
 define( 'BCM_DIRNAME', $bcm_dirname );
 define( 'BCM_UPLOADS_PATH', $wp_uploads_path );
 define( 'BCM_UPLOADS_URL', $wp_uploads_url );
 define( 'BCM_EXAMPLE_FILES_PATH', $example_files_path );
+define('BCM_VERSION', '0.1.0');
 
 /**
  * BC Migration class.
