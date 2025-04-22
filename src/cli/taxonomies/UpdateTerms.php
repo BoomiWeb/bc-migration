11a0359612311ea32b339667e77e810433725f38
Erik Mitchell <erikdmitchell@gmail.com>
2025-04-22 11:26:03 -0400

    added header to csv, update terms bulk works

--- src/cli/taxonomies/UpdateTerms.php
+++ src/cli/taxonomies/UpdateTerms.php
@@ -120,15 +120,16 @@ class UpdateTerms extends TaxonomyCLICommands {
         $rows    = array_map( 'str_getcsv', file( $file ) );
         $headers = array_map( 'trim', array_shift( $rows ) );
 
-        if ( ! $this->validate_headers( $headers, array( 'taxonomy', 'from_terms', 'to_term' ) ) ) {
+        if ( ! $this->validate_headers( $headers, array( 'taxonomy', 'terms' ) ) ) {
             return;
         }   
-        
+
         foreach ( $rows as $i => $row ) {
-            $row_num  = $i + 2;
+            $row_num  = $i + 2;            
             $data     = array_combine( $headers, $row );
             $data     = array_map( 'trim', $data );
             $taxonomy = $data['taxonomy'];
+            $mappings = array();
 
             // skip empty lines.
             if ( count( $row ) === 1 && empty( trim( $row[0] ) ) ) {
@@ -148,52 +149,36 @@ class UpdateTerms extends TaxonomyCLICommands {
                 continue;
             }
 
-            print_r( $data );
+            $parts = explode( '>', $data['terms'] );
 
-            // $from_terms = explode( '|', $data['from_terms'] );
-            // $to_term    = $data['to_term'];
+            if ( count( $parts ) !== 2 ) {
+                $this->add_notice( "Invalid format in line: {$row_num}", 'warning' );
+                $this->log( "Invalid format in line: {$row_num}" );
 
-            // if ( $dry_run ) {
-            //     $this->dry_run_result( $taxonomy, $from_terms, $to_term, $row_num );
+                continue;
+            }  
+            
+            $parent     = trim( $parts[0] );
+            $children   = array_map( 'trim', explode( ',', $parts[1] ) );
+            $mappings[] = array(
+                'parent'   => $parent,
+                'children' => $children,
+            );
 
-            //     continue;
-            // }
+            if ( $dry_run ) {
+                // $this->dry_run_result( $taxonomy, $from_terms, $to_term, $row_num );
+                $this->add_notice( "Row $row_num: Dry run - No changes made.", 'info' );
+                $this->log( "Row $row_num: Dry run - No changes made." );
 
-            // $result = $this->merge( $taxonomy, $from_terms, $to_term, $delete_old, $row_num, $post_type );
+                continue;
+            }
 
-            // if ( is_wp_error( $result ) ) {
-            //     $this->add_notice( "Row $row_num: Error - " . $result->get_error_message(), 'warning' );
-            // }
-        }        
+            $result = $this->process_terms( $mappings, $taxonomy, $dry_run );
 
-        // foreach ( $lines as $line ) {
-        //     $parts = explode( '>', $line );
-
-        //     if ( count( $parts ) !== 2 ) {
-        //         $this->add_notice( "Invalid format in line: {$line}", 'warning' );
-        //         $this->log( "Invalid format in line: {$line}" );
-
-        //         continue;
-        //     }
-
-        //     $parent     = trim( $parts[0] );
-        //     $children   = array_map( 'trim', explode( ',', $parts[1] ) );
-        //     $mappings[] = array(
-        //         'parent'   => $parent,
-        //         'children' => $children,
-        //     );
-        // }
-
-        // if ( empty( $mappings ) ) {
-        //     $this->add_notice( 'No valid mappings found in CSV file.', 'error' );
-        //     $this->log( 'No valid mappings found in CSV file.' );
-
-        //     $this->display_notices();
-
-        //     return;
-        // }
-
-        // $this->process_terms( $mappings, $taxonomy, $dry_run );
+            if ( is_wp_error( $result ) ) {
+                $this->add_notice( "Row $row_num: Error - " . $result->get_error_message(), 'warning' );
+            }
+        }
     }
 
     private function process_single_term( array $args, bool $dry_run ) {
